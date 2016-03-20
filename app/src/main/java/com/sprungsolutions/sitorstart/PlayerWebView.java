f9f334d51291cd1b46d4d2ba16d6185ef6c0a814
#{arisprung} <#{arisprung@gmail.com}>
2016-03-20 22:53:35 +0200

    web view

--- app/src/main/java/com/sprungsolutions/sitorstart/PlayerWebView.java
+++ app/src/main/java/com/sprungsolutions/sitorstart/PlayerWebView.java
@@ -17,6 +17,7 @@ import dmax.dialog.SpotsDialog;
 public class PlayerWebView extends AppCompatActivity {
 
     private WebView mWebView;
+    private SpotsDialog mDialog;
 
    // private SpotsDialog mDialog;
 
@@ -26,11 +27,15 @@ public class PlayerWebView extends AppCompatActivity {
         setContentView(R.layout.player_web_view_layout);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_web);
         setSupportActionBar(toolbar);
+//        mDialog = new SpotsDialog(PlayerWebView.this,R.style.CustomPlayer);
+//
+//        mDialog.show();
+
         String url = getIntent().getStringExtra("player_url");
         mWebView=(WebView)findViewById(R.id.player_webview);
-        mWebView.setWebViewClient(new WebViewClient());
-//        WebSettings webSettings = mWebView.getSettings();
-//        webSettings.setJavaScriptEnabled(true);
+        mWebView.setWebViewClient(new myWebClient());
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
 //        webSettings.setCacheMode(WebSettings.LOAD_DEFAULT);
         mWebView.loadUrl(url);
 
@@ -42,11 +47,33 @@ public class PlayerWebView extends AppCompatActivity {
         super.onResume();
 
         String name = getIntent().getStringExtra("player_name");
-//        mDialog = new SpotsDialog(PlayerWebView.this);
-//        mDialog.setMessage("Downloading "+name+" data...");
-//        mDialog.show();
 
 
+
+    }
+
+    public class myWebClient extends WebViewClient
+    {
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            // TODO Auto-generated method stub
+            super.onPageStarted(view, url, favicon);
+        }
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            // TODO Auto-generated method stub
+
+            view.loadUrl(url);
+            return true;
+
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+
+
+        }
     }
 
 }
