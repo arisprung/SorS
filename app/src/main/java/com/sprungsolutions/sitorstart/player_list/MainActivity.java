b661e10483e218a029ceea065b25c13a2ed08202
#{arisprung} <#{arisprung@gmail.com}>
2016-03-27 00:17:44 +0300

    tutorial

--- app/src/main/java/com/sprungsolutions/sitorstart/player_list/MainActivity.java
+++ app/src/main/java/com/sprungsolutions/sitorstart/player_list/MainActivity.java
@@ -2,6 +2,7 @@ package com.sprungsolutions.sitorstart.player_list;
 
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
@@ -9,43 +10,63 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.widget.Toast;
 
+import com.daimajia.swipe.SwipeLayout;
 import com.daimajia.swipe.util.Attributes;
 import com.firebase.client.DataSnapshot;
 import com.firebase.client.FirebaseError;
 import com.firebase.client.ValueEventListener;
-import com.sprungsolutions.sitorstart.pick_player.AddPlayerSetFragment;
-import com.sprungsolutions.sitorstart.pick_player.NewPlayerSet;
 import com.sprungsolutions.sitorstart.R;
 import com.sprungsolutions.sitorstart.application.SitOrStartApplication;
+import com.sprungsolutions.sitorstart.pick_player.AddPlayerSetFragment;
+import com.sprungsolutions.sitorstart.pick_player.NewPlayerSet;
 import com.sprungsolutions.sitorstart.utility.SSSharedPreferencesManager;
 import com.sprungsolutions.sitorstart.utility.SitStartUtility;
 import com.sprungsolutions.sitorstart.views.DividerItemDecoration;
-
 
 import java.util.ArrayList;
 import java.util.Collections;
 
 import cn.pedant.SweetAlert.SweetAlertDialog;
 import dmax.dialog.SpotsDialog;
+import tourguide.tourguide.ChainTourGuide;
+import tourguide.tourguide.Overlay;
+import tourguide.tourguide.Sequence;
+import tourguide.tourguide.ToolTip;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView mRecyclerView;
     private Handler handler = new Handler();
-    private RecyclerView.Adapter mAdapter;
+    private SSRecyclerViewAdapter mAdapter;
     private ArrayList<NewPlayerSet> mPlayerList;
+
+    public ChainTourGuide mTutorialHandler;
+    public Sequence mSequence;
 
     private SSSharedPreferencesManager ssPrefrenceManager;
     private SpotsDialog mDialog;
+    private FloatingActionButton mFab;
+    private Animation mEnterAnimation, mExitAnimation;
 
+    private View mSlidedView;
+    private View mSelectedView;
+
+    private boolean stopSelectDelay;
+    private boolean stopswipeDelay;
+
+    private int mTimesTutorialNext = 0;
 
 
     @Override
@@ -60,6 +81,15 @@ public class MainActivity extends AppCompatActivity {
                     .show();
         }
 
+         /* setup enter and exit animation */
+        mEnterAnimation = new AlphaAnimation(0f, 1f);
+        mEnterAnimation.setDuration(600);
+        mEnterAnimation.setFillAfter(true);
+
+        mExitAnimation = new AlphaAnimation(1f, 0f);
+        mExitAnimation.setDuration(600);
+        mExitAnimation.setFillAfter(true);
+
         ssPrefrenceManager = SSSharedPreferencesManager.getInstance(getApplicationContext());
         mDialog = new SpotsDialog(MainActivity.this, R.style.Custom);
         mDialog.show();
@@ -70,8 +100,10 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        mFab = (FloatingActionButton) findViewById(R.id.fab);
+
+
+        mFab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 //                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
@@ -88,6 +120,7 @@ public class MainActivity extends AppCompatActivity {
                         "action_bar_frag");
             }
         });
+
 
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
@@ -108,7 +141,151 @@ public class MainActivity extends AppCompatActivity {
         loadPLayersSetFromFireBase();
 
 
+    }
 
+    private void setupTutorial() {
+
+
+        ViewGroup view = ((ViewGroup) mRecyclerView.getChildAt(0));
+        ViewGroup view1 = ((ViewGroup) view.getChildAt(0));
+        ViewGroup view2 = ((ViewGroup) view1.getChildAt(1));
+        ViewGroup view3 = ((ViewGroup) view2.getChildAt(0));
+
+        mSlidedView = view3.getChildAt(1);
+
+        ViewGroup sview = ((ViewGroup) mRecyclerView.getChildAt(2));
+        ViewGroup sview1 = ((ViewGroup) sview.getChildAt(0));
+        ViewGroup sview2 = ((ViewGroup) sview1.getChildAt(1));
+        ViewGroup sview3 = ((ViewGroup) sview2.getChildAt(0));
+
+        mSelectedView = sview3.getChildAt(1);
+
+        ChainTourGuide mTG1 = ChainTourGuide.init(this)
+                .setToolTip(new ToolTip()
+                        .setTitle("Swipe To see result!")
+                        .setDescription("See the result of the challange")
+                        .setGravity(Gravity.BOTTOM))
+                .setOverlay(new Overlay()
+                        .setEnterAnimation(mEnterAnimation).setExitAnimation(mEnterAnimation))
+                .playLater(mFab);
+
+
+        ChainTourGuide mTG2 = ChainTourGuide.init(this)
+                .setToolTip(new ToolTip()
+                        .setTitle("Select PLayer to start!")
+                        .setDescription("Select who you think should start and help the fantasy world")
+                        .setGravity(Gravity.BOTTOM))
+                .playLater(mFab);
+
+        ChainTourGuide mTG3 = ChainTourGuide.init(this)
+                .setToolTip(new ToolTip()
+                        .setTitle("Add Set!")
+                        .setDescription("Want to start your own set presss +")
+                        .setGravity(Gravity.TOP | Gravity.LEFT))
+                .playLater(mFab);
+
+
+//        mSequence = new Sequence.SequenceBuilder()
+//                .add(mTG1, mTG2,mTG3)
+//                .setDefaultOverlay(new Overlay()
+//                        .setEnterAnimation(mEnterAnimation).setExitAnimation(mExitAnimation)
+//                        .setOnClickListener(new View.OnClickListener() {
+//                            @Override
+//                            public void onClick(View v) {
+////                                mTutorialHandler.next();
+////                                if(mTimesTutorialNext == 0){
+////                                    mTutorialHandler.next();
+////
+//////                                    final Handler handler = new Handler();
+//////                                    handler.postDelayed(new Runnable() {
+//////                                        @Override
+//////                                        public void run() {
+//////
+//////                                            selectAndDeselctLayout();
+//////
+//////
+//////                                        }
+//////                                    }, 500);
+////                                } else{
+////                                    mTutorialHandler.next();
+////                                }
+////
+////                                mTimesTutorialNext++;
+//
+//                            }
+//                        }))
+//
+//                .setDefaultPointer(new Pointer())
+//                .setContinueMethod(Sequence.ContinueMethod.OverlayListener)
+//                .build();
+//
+//        mTutorialHandler = ChainTourGuide.init(this)
+//                .playInSequence(mSequence);
+
+
+//        final Handler handler = new Handler();
+//        handler.postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//
+//                swipeAndCloseLayout();
+//
+//
+//            }
+//        }, 500);
+
+        Sequence sequence = new Sequence.SequenceBuilder()
+                .add(mTG1, mTG2, mTG3)
+                .setDefaultOverlay(new Overlay()
+                        .setEnterAnimation(mEnterAnimation)
+                        .setExitAnimation(mExitAnimation)
+                        .setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Toast.makeText(MainActivity.this, "default Overlay clicked", Toast.LENGTH_SHORT).show();
+                                mTutorialHandler.next();
+                            }
+                        })
+                )
+                .setDefaultPointer(null)
+                .setContinueMethod(Sequence.ContinueMethod.OverlayListener)
+                .build();
+
+        mTutorialHandler = ChainTourGuide.init(this).playInSequence(sequence);
+
+
+    }
+
+    private void selectAndDeselctLayout() {
+
+
+        mSelectedView.setBackgroundResource(SitOrStartApplication.getInstance().getSelectorResourceId(2, true)[1]);
+
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mSelectedView.setBackgroundResource(SitOrStartApplication.getInstance().getSelectorResourceId(2, false)[1]);
+                // mTutorialHandler.next();
+
+            }
+        }, 1000);
+    }
+
+    private void swipeAndCloseLayout() {
+        final SwipeLayout fullFrontView = (SwipeLayout) ((ViewGroup) mRecyclerView.getChildAt(0)).getChildAt(0);
+        fullFrontView.open();
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                fullFrontView.close();
+
+
+            }
+        }, 1000);
     }
 
     /**
@@ -137,7 +314,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -159,17 +335,15 @@ public class MainActivity extends AppCompatActivity {
                         Uri.parse("http://play.google.com/store/apps/details?id=" + getApplicationContext().getPackageName())));
             }
             return true;
-        } else if(id == R.id.menu_item_share){
+        } else if (id == R.id.menu_item_share) {
             Intent share = new Intent(Intent.ACTION_SEND);
             share.setType("text/plain");
-            share.putExtra(Intent.EXTRA_TEXT, "Want to win your fantasy matchup! Download this app now! "+"http://play.google.com/store/apps/details?id=" + getApplicationContext().getPackageName());
+            share.putExtra(Intent.EXTRA_TEXT, "Want to win your fantasy matchup! Download this app now! " + "http://play.google.com/store/apps/details?id=" + getApplicationContext().getPackageName());
             startActivity(Intent.createChooser(share, "share"));
         }
 
         return super.onOptionsItemSelected(item);
     }
-
-
 
 
     private void loadPLayersSetFromFireBase() {
@@ -245,7 +419,162 @@ public class MainActivity extends AppCompatActivity {
             } else {
                 mDialog.dismiss();
                 handler.removeCallbacks(runnable);
+                //setupTutorial();
+                runOverlayListener_ContinueMethod();
+
             }
         }
     };
+
+    private void runOverlayListener_ContinueMethod() {
+
+        ViewGroup view = ((ViewGroup) mRecyclerView.getChildAt(0));
+        ViewGroup view1 = ((ViewGroup) view.getChildAt(0));
+        ViewGroup view2 = ((ViewGroup) view1.getChildAt(1));
+        mSlidedView=  view2.getChildAt(0);
+
+      //  mSlidedView = view3.getChildAt(1);
+
+        ViewGroup sview = ((ViewGroup) mRecyclerView.getChildAt(2));
+        ViewGroup sview1 = ((ViewGroup) sview.getChildAt(0));
+        ViewGroup sview2 = ((ViewGroup) sview1.getChildAt(1));
+        ViewGroup sview3 = ((ViewGroup) sview2.getChildAt(0));
+
+        mSelectedView = sview3.getChildAt(1);
+        // the return handler is used to manipulate the cleanup of all the tutorial elements
+        ChainTourGuide tourGuide1 = ChainTourGuide.init(this)
+                .setToolTip(new ToolTip()
+                        .setTitle("Swipe To see result!")
+                        .setDescription("See the result of the challange")
+                        .setGravity(Gravity.BOTTOM)
+                )
+                .setOverlay(new Overlay()
+                        .setEnterAnimation(mEnterAnimation)
+                        .setExitAnimation(mExitAnimation)
+                        .setBackgroundColor(Color.parseColor("#EE2c3e50"))
+                        .setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                stopswipeDelay = true;
+                                mTutorialHandler.next();
+                                final Handler handler = new Handler();
+                                handler.postDelayed(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        //Do something after 100ms
+                                        playSelectDeselectTutorial();
+                                        if(!stopSelectDelay){
+                                            handler.postDelayed(this, 2000);
+                                        }
+
+                                    }
+                                }, 500);
+
+
+
+
+                            }
+                        })
+                )
+                // note that there is not Overlay here, so the default one will be used
+                .playLater(mSlidedView);
+
+        ChainTourGuide tourGuide2 = ChainTourGuide.init(this)
+                .setToolTip(new ToolTip()
+                        .setTitle("Select PLayer to start!")
+                        .setDescription("Select who you think should start and help the fantasy world")
+                        .setBackgroundColor(Color.parseColor("#c0392b"))
+                        .setGravity(Gravity.BOTTOM)
+                )
+                .setOverlay(new Overlay()
+                        .setEnterAnimation(mEnterAnimation)
+                        .setExitAnimation(mExitAnimation)
+                        .setBackgroundColor(Color.parseColor("#EE2c3e50"))
+                        .setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                mTutorialHandler.next();
+                                stopSelectDelay = true;
+                            }
+                        })
+                )
+                .playLater(mSelectedView);
+
+        ChainTourGuide tourGuide3 = ChainTourGuide.init(this)
+                .setToolTip(new ToolTip()
+                        .setTitle("Add Set!")
+                        .setDescription("Want to start your own set presss +")
+                        .setGravity(Gravity.TOP)
+                )
+                // note that there is not Overlay here, so the default one will be used
+                .playLater(mFab);
+
+        Sequence sequence = new Sequence.SequenceBuilder()
+                .add(tourGuide1, tourGuide2, tourGuide3)
+                .setDefaultOverlay(new Overlay()
+                        .setEnterAnimation(mEnterAnimation)
+                        .setExitAnimation(mExitAnimation)
+                        .setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                mTutorialHandler.next();
+                            }
+                        })
+                )
+                .setDefaultPointer(null)
+                .setContinueMethod(Sequence.ContinueMethod.OverlayListener)
+                .build();
+
+        mTutorialHandler = ChainTourGuide.init(this).playInSequence(sequence);
+
+
+//        final Handler handler = new Handler();
+//        handler.postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//
+//                swipeAndCloseLayout();
+//
+//
+//            }
+//        }, 1000);
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //Do something after 100ms
+                swipeAndCloseLayout();
+                if(!stopswipeDelay){
+                    handler.postDelayed(this, 2000);
+                }
+
+            }
+        }, 1000);
+    }
+
+    private void playSelectDeselectTutorial() {
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                selectAndDeselctLayout();
+
+
+            }
+        }, 500);
+    }
+//    private void playSlideUnslideTutorial() {
+//        final Handler handler = new Handler();
+//        handler.postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//
+//                swipeAndCloseLayout();
+//
+//
+//            }
+//        }, 500);
+//    }
 }
--- app/src/main/java/com/sprungsolutions/sitorstart/player_list/SSRecyclerViewAdapter.java
+++ app/src/main/java/com/sprungsolutions/sitorstart/player_list/SSRecyclerViewAdapter.java
@@ -173,19 +173,6 @@ public class SSRecyclerViewAdapter extends RecyclerSwipeAdapter<SSRecyclerViewAd
     public void onBindViewHolder(final SimpleViewHolder viewHolder, final int position) {
         NewPlayerSet item = mDataset.get(position);
         viewHolder.swipeLayout.setShowMode(SwipeLayout.ShowMode.LayDown);
-//        viewHolder.swipeLayout.addSwipeListener(new SimpleSwipeListener() {
-//            @Override
-//            public void onOpen(SwipeLayout layout) {
-//               // YoYo.with(Techniques.Tada).duration(500).delay(100).playOn(layout.findViewById(R.id.trash));
-//            }
-//        });
-//        viewHolder.swipeLayout.setOnDoubleClickListener(new SwipeLayout.DoubleClickListener() {
-//            @Override
-//            public void onDoubleClick(SwipeLayout layout, boolean surface) {
-//                Toast.makeText(mContext, "DoubleClick", Toast.LENGTH_SHORT).show();
-//            }
-//        });
-
 
         viewHolder.mName1.setText(item.getmPlayer1().getMlb_name());
         viewHolder.mName2.setText(item.getmPlayer2().getMlb_name());
@@ -224,15 +211,6 @@ public class SSRecyclerViewAdapter extends RecyclerSwipeAdapter<SSRecyclerViewAd
         }
 
         int total = item.getmPlayer1().getScore() + item.getmPlayer2().getScore();
-//        float percent1 = 0.1f;
-//        if(item.getMlb_set_score_1()!=0){
-//            percent1 = (total * 100.0f) / item.getMlb_set_score_1();
-//        }
-//
-//        float percent2 = 0.10f;
-//        if(item.getMlb_set_score_2()!=0){
-//            percent2 = (total * 100.0f) / item.getMlb_set_score_2();
-//        }
 
 
         float percent1 = (item.getmPlayer1().getScore() * 100.0f) / (total);
